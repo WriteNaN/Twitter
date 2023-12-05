a4722913f489a09a2702211cb83f94aabd7ff50c
Celio Sevii <151211283+WriteNaN@users.noreply.github.com>
2023-12-05 15:09:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,24 @@ const { promisify } = require("node:util");
 const fs = require("fs");
 const path = require("path");
 const EventEmitter = require("events");
+const { launch, getStream } = require("puppeteer-stream");
 
-class Twitter extends EventEmitter {
-  constructor(cookiePath) {
+class Client extends EventEmitter {
+  constructor(cookiePath, {timeout, userAgent}) {
     super();
     this.xpaths = require("./xpath.json");
     this.defaultType = { delay: 30 };
     this.sleep = (waitTimeInMs) =>
       new Promise((resolve) => setTimeout(resolve, waitTimeInMs));
-    this._init(cookiePath);
+    this._init(cookiePath, userAgent);
   }
 
-  async _init(cookiePath) {
+  async _init(cookiePath, userAgent=('Mozilla/5.0 (X11; Linux x86_64)'+'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36')) {
     const { stdout: chromiumPath } = await promisify(exec)("which chromium");
     //console.log("found chromium");
     this.executablePath = chromiumPath.trim();
 
-    const cookiesString = await fs.promises.readFile(cookiePath) || await fs.promises.readFile(
-      path.join(__dirname, "cookies.json")
-    );
+    const cookiesString = await fs.promises.readFile(cookiePath);
     this.cookies = JSON.parse(cookiesString);
 
     this.browser = await puppeteer.launch({
@@ -38,11 +37,35 @@ class Twitter extends EventEmitter {
       await this.page.setCookie(cookie);
     }
 
+    await this.page.setUserAgent(userAgent);
+
     await this.page.goto("https://twitter.com/home");
+
+    await this.page.waitForSelector(this.xpaths.profile);
+    await this.page.click(this.xpaths.profile);
+    const usernameHandle = await this.page.waitForXPath(this.xpaths.username_span);
+    const displayNameHandle = await this.page.waitForXPath(this.xpaths.displayname_span);
+    const displayAboutHandle = await this.page.waitForXPath(this.xpaths.display_about);
+    const displayImageHandle = await this.page.waitForXPath(this.xpaths.display_image);
+    const displayBannerHandle = await this.page.waitForXPath(this.xpaths.display_banner);
+    const username = usernameHandle ? await this.page.evaluate(element => element.innerHTML, usernameHandle) : null;
+    const displayName = displayNameHandle ? await this.page.evaluate(element => element.innerHTML, displayNameHandle) : null;
+    const displayAbout = displayAboutHandle ? await this.page.evaluate(element => element.innerText.trim(), displayAboutHandle): null;
+    const displayImage = displayImageHandle ? await this.page.evaluate(element => element.style.backgroundImage.slice(4, -1).replace(/"/g, ""), displayImageHandle): null;
+    const displayBanner = displayBannerHandle ? await this.page.evaluate(element => element.src, displayBannerHandle) : null;
+    
+
+    this.user = {
+      username,
+      displayName,
+      displayAbout: displayAbout.replace(/\n/g, ""),
+      displayImage,
+      displayBanner
+    };
+    return this.emit('ready', true);
   }
 
   async takeScreenshot() {
-    await new Promise((res) => setTimeout(res, 2000));
     await this.page.screenshot({ path: "screenshot.png" });
   }
 
@@ -51,15 +74,17 @@ class Twitter extends EventEmitter {
     process.exit(0);
   }
 
-  async tweet({ content, media, poll, mediaTimeout }) {
-    if (!mediaTimeout) mediaTimeout = 1000;
+  async tweet({ content, media, poll, mediaTimeout = 2000 }) {
     if (media && poll)
       return console.error("Only one of media or poll can exist in one tweet");
 
     const activeURL = await this.page.url();
     const url = `https://twitter.com/home`;
 
-    if (activeURL !== url) await this.page.goto(url);
+    if (activeURL !== url) {
+      await this.page.goto(url);
+      await this.page.waitForNetworkIdle();
+    }
 
     await this.page.waitForXPath(this.xpaths.tweet_div);
     const tweetModal = await this.page.$x(this.xpaths.tweet_modal);
@@ -128,13 +153,59 @@ class Twitter extends EventEmitter {
       //await this.page.screenshot({path: "screenshot.png"});
     }
 
-    const nextButton = await this.page.waitForXPath(this.xpaths.tweet_enter, { visible: true });
-    await nextButton.click();
+    // Replace the above lines with this code to wait for the button to be enabled and then click
+    await this.page.waitForFunction(
+      xpath => {
+        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        return element && !element.disabled;
+      },
+      {},
+      this.xpaths.tweet_enter
+    );
+    const [tweetButton] = await this.page.$x(this.xpaths.tweet_enter);
+    if (tweetButton) {
+      await tweetButton.click();
+      await this.sleep(1000);
+      try {await tweetButton.click();} catch {};
+    } else {
+      throw new Error("Tweet button not found");
+    }
 
-    await this.sleep(500);
+    await this.sleep(3000);
+
+    await this.page.screenshot({ path: "screenshot.png" });
 
     return true;
   }
+
+  async fetchProfile(username) {
+    await this.page.goto(`https://twitter.com/${username}`);
+    const usernameHandle = await this.page.waitForXPath(this.xpaths.username_span);
+    const displayNameHandle = await this.page.waitForXPath(this.xpaths.displayname_span);
+    const displayAboutHandle = await this.page.waitForXPath(this.xpaths.display_about);
+    const displayImageHandle = await this.page.waitForXPath(this.xpaths.display_image);
+    const displayBannerHandle = await this.page.waitForXPath(this.xpaths.display_banner);
+    const usernamex = usernameHandle ? await this.page.evaluate(element => element.innerHTML, usernameHandle) : null;
+    const displayName = displayNameHandle ? await this.page.evaluate(element => element.innerHTML, displayNameHandle) : null;
+    const displayAbout = displayAboutHandle ? await this.page.evaluate(element => element.innerText.trim(), displayAboutHandle): null;
+    const displayImage = displayImageHandle ? await this.page.evaluate(element => element.style.backgroundImage.slice(4, -1).replace(/"/g, ""), displayImageHandle): null;
+    const displayBanner = displayBannerHandle ? await this.page.evaluate(element => element.src, displayBannerHandle) : null;
+
+
+    const user = {
+      username: usernamex,
+      displayName,
+      displayAbout: displayAbout.replace(/\n/g, ""),
+      displayImage,
+      displayBanner
+    };
+
+    return user;
+  }
+
+  async getStream(){
+    await getStream(this.page, { audio: true, video: true });
+  }
 }
 
-module.exports = Twitter;
+module.exports = {Client};
